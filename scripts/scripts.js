ea889d42bb5bf09fa55501be1748211884112d00
root <Leandergs1234@gmail.com>
2022-03-05 17:32:26 -0400

    I updated my website with new 'inputs' and removed the 'prompt'

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -12,56 +12,102 @@ let container = document.getElementById("containerResult");
 let button = document.getElementById("eventButton");
 
 function yourWeightInOhterPlanet() {
-    let userWeight = parseFloat(prompt("Hi!, what is your weight?"));
 
-    let planet = parseInt(prompt("I want to know how much weight in...\n\n 1.- Mercury\n 2.- Venus\n 3.- Earth\n 4.- Mars\n 5.- Jupiter\n 6.- Saturn\n 7.- Uranus\n 8.- Neptune")); 
+    const inputWeight = document.getElementById("weight");
+    const inputPlanet = document.getElementById("planet");
 
-    if(planet == 1) {
-        let weightResult = userWeight * gMercury / gEarth;
-        weightResult = parseFloat(weightResult);
-        weightResult = weightResult.toFixed(2);
-        container.innerHTML = `<p><strong>Your weight on Mercury is:</strong> ${weightResult} kilos, amazing! 😲</p>\n<h3>Thank you very much for trusting in us ♥</h3>`;
+    let userWeight = parseFloat(inputWeight.value);
+    let planet = inputPlanet.value; 
+
+    let weightResult;
+
+    switch(true) { 
+
+        case planet == "mercury" && userWeight > 0:
+            weightResult = userWeight * gMercury / gEarth;
+            weightResult = parseFloat(weightResult);
+            weightResult = weightResult.toFixed(2);
+            container.innerHTML = `<p><strong>Your weight on Mercury is:</strong> ${weightResult} kilos, amazing! 😲</p>\n<h3>Thank you very much for trusting in us ♥</h3>`;
+            break;
+
+        case planet == "venus" && userWeight > 0:
+            weightResult = userWeight * gVenus / gEarth;
+            weightResult = weightResult.toFixed(2);
+            container.innerHTML = `<p><strong>Your weight on Venus is:</strong> ${weightResult} kilos, amazing! 😲</p>\n<h3>Thank you very much for trusting in us ♥</h3>`;
+            break;
+
+        case planet == "earth" && userWeight > 0:
+            weightResult = userWeight * gEarth / gEarth;
+            weightResult = weightResult.toFixed(2);
+            container.innerHTML = `<p><strong>Your weight on the Earth is:</strong> ${weightResult} kilos, amazing! 😲</p>\n<h3>Thank you very much for trusting in us ♥</h3>`;
+            break;
+
+        case planet == "mars" && userWeight > 0:
+            weightResult = userWeight * gMars / gEarth;
+            weightResult = weightResult.toFixed(2);
+            container.innerHTML = `<p><strong>Your weight on Mars is:</strong> ${weightResult} kilos, amazing! 😲</p>\n<h3>Thank you very much for trusting in us ♥</h3>`;
+            break;
+
+        case planet == "jupiter" && userWeight > 0:
+            weightResult = userWeight * gJupiter / gEarth;
+            weightResult = weightResult.toFixed(2);
+            container.innerHTML = `<p><strong>Your weight on Jupiter is:</strong> ${weightResult} kilos, amazing! 😲</p>\n<h3>Thank you very much for trusting in us ♥</h3>`;
+            break;
+
+        case planet == "saturn" && userWeight > 0:
+            weightResult = userWeight * gSaturn / gEarth;
+            weightResult = weightResult.toFixed(2);
+            container.innerHTML = `<p><strong>Your weight on Saturn is:</strong> ${weightResult} kilos, amazing! 😲</p>\n<h3>Thank you very much for trusting in us ♥</h3>`;
+            break;
+
+        case planet == "uranus" && userWeight > 0:
+            weightResult = userWeight * gUranus / gEarth;
+            weightResult = weightResult.toFixed(2);
+            container.innerHTML = `<p><strong>Your weight on Uranus is:</strong> ${weightResult} kilos, amazing! 😲</p>\n<h3>Thank you very much for trusting in us ♥</h3>`;
+            break;
+
+        case planet == "neptune" && userWeight > 0:
+            weightResult = userWeight * gNeptune / gEarth;
+            weightResult = weightResult.toFixed(2);
+            container.innerHTML = `<p><strong>Your weight on Neptune is:</strong> ${weightResult} kilos, amazing! 😲</p>\n<h3>Thank you very much for trusting in us ♥</h3>`;
+            break;
+
+        case planet == "kaiosama" && userWeight > 0:
+            weightResult = userWeight * gPK / gEarth;
+            weightResult = weightResult.toFixed(2);
+            container.innerHTML = `<p><strong>Your weight on the Kaio-sama planet is:</strong> ${weightResult} kilos, amazing! 😲</p>\n<h3>Thank you very much for trusting in us ♥</h3>`;
+            break;    
+
+        default:
+            container.innerHTML = `<p><strong>Please, enter a value greater than 0 in your weight.</h3>`;
+    
     }
-    else if(planet == 2){
-        let weightResult = userWeight * gVenus / gEarth;
-        weightResult = weightResult.toFixed(2);
-        container.innerHTML = `<p><strong>Your weight on Venus is:</strong> ${weightResult} kilos, amazing! 😲</p>\n<h3>Thank you very much for trusting in us ♥</h3>`;
-    }
-    else if(planet == 3){
-        let weightResult = userWeight * gEarth / gEarth;
-        weightResult = weightResult.toFixed(2);
-        container.innerHTML = `<p><strong>Your weight on the Earth is:</strong> ${weightResult} kilos, amazing! 😲</p>\n<h3>Thank you very much for trusting in us ♥</h3>`;
-    }
-    else if(planet == 4){
-        let weightResult = userWeight * gMars / gEarth;
-        weightResult = weightResult.toFixed(2);
-        container.innerHTML = `<p><strong>Your weight on Mars is:</strong> ${weightResult} kilos, amazing! 😲</p>\n<h3>Thank you very much for trusting in us ♥</h3>`;
-    }
-    else if(planet == 5){
-        let weightResult = userWeight * gJupiter / gEarth;
-        weightResult = weightResult.toFixed(2);
-        container.innerHTML = `<p><strong>Your weight on Jupiter is:</strong> ${weightResult} kilos, amazing! 😲</p>\n<h3>Thank you very much for trusting in us ♥</h3>`;
-    }
-    else if(planet == 6){
-        let weightResult = userWeight * gSaturn / gEarth;
-        weightResult = weightResult.toFixed(2);
-        container.innerHTML = `<p><strong>Your weight on Saturn is:</strong> ${weightResult} kilos, amazing! 😲</p>\n<h3>Thank you very much for trusting in us ♥</h3>`;
-    }
-    else if(planet == 7){
-        let weightResult = userWeight * gUranus / gEarth;
-        weightResult = weightResult.toFixed(2);
-        container.innerHTML = `<p><strong>Your weight on Uranus is:</strong> ${weightResult} kilos, amazing! 😲</p>\n<h3>Thank you very much for trusting in us ♥</h3>`;
-    }
-    else if(planet == 8){
-        let weightResult = userWeight * gNeptune / gEarth;
-        weightResult = weightResult.toFixed(2);
-        container.innerHTML = `<p><strong>Your weight on Neptune is:</strong> ${weightResult} kilos, amazing! 😲</p>\n<h3>Thank you very much for trusting in us ♥</h3>`;
-    }
-    else {
-        let weightResult = userWeight * gPK / gEarth;
-        weightResult = weightResult.toFixed(2);
-        container.innerHTML = `<p><strong>Your weight on the Kaio-sama planet is:</strong> ${weightResult} kilos, amazing! 😲</p>\n<h3>This answer is result of a error. Please you enter a value correct from 1 to 8.</h3>`;
-    } 
+    // if(planet == 1) {
+        
+    // }
+    // else if(planet == 2){
+    //     ;
+    // }
+    // else if(planet == 3){
+    //     let 
+    // }
+    // else if(planet == 4){
+    //     let 
+    // }
+    // else if(planet == 5){
+    //     let 
+    // }
+    // else if(planet == 6){
+    //     let 
+    // }
+    // else if(planet == 7){
+    //     let 
+    // }
+    // else if(planet == 8){
+    //     let 
+    // }
+    // else {
+    //     let } 
 }
 
 button.addEventListener("click", yourWeightInOhterPlanet);
